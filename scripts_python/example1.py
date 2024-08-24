c0b3fddf6e573a0abe08084ed2c13c4965cb92a9
Juan <pabloerazo1505@gmail.com>
2024-08-23 21:01:25 -0500

    Script python 1

---
+++ scripts_python/example1.py
@@ -0,0 +1,12 @@
+#Este es un comentario
+'''
+Esto es un coment of bloque
+'''
+n1 =10
+n2= 20
+suma= n1+n2
+
+print (suma)
+print ("La suma es: ", suma)
+print (f"La suma es: {suma}")
+print ("La suma es: ",n1+n2)
